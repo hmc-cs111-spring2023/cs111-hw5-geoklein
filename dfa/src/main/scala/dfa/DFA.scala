ec2822e353f4e1ac6301a327007fd10fdd248442
Georgia Klein <georgia.r.klein@gmail.com>
2023-02-19 00:07:22 +0000

    Remove TODO comment

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -1,7 +1,5 @@
 package dfa // leave this line in the file
 
-// TODO: replace this comment with your implementation
-
 case class State(label: String)
 case class Transition(from: State, to: State, symbol: Char)
 
